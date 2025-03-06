5bff0ebff77e7317d9ad1e6c7a6e40693dc4d455
AsaBizanjo <asabizenjo3@gmail.com>
2025-03-06 01:54:19 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/image-generation/' : '/', 
+  base: mode === 'production' ? '/Image-Generation/' : '/', 
   server: {
     host: "::",
     port: 8080,
